bbe516468456672908c514905cea7758ece0d65f
Srinidhi <srinidhismurthy@gmail.com>
2021-02-17 20:17:59 +0530

    m2

--- 2.py
+++ 2.py
@@ -2,3 +2,5 @@ print("#####################################################")
 print("Git Actions working fine on Git Basics Repo")
 print("#####################################################")
 print("Modification 1")
+print("modification 2")
+
